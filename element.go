398a692d0a10e5650957599af88f80de19030c2b
Sergey Peshkov <serge1peshcoff@mail.ru>
2017-08-23 13:42:26 +0300

    ElementIsDisplayed() function and tests

--- element.go
+++ element.go
@@ -4,10 +4,18 @@ import (
 	"github.com/tebeka/selenium"
 )
 
-// ElementIsLocated returns a condition that checks if theelement is found on page.
+// ElementIsLocated returns a condition that checks if the element is found on page.
 func ElementIsLocated(by, selector string) selenium.Condition {
 	return func(wd selenium.WebDriver) (bool, error) {
 		_, err := wd.FindElement(by, selector)
 		return err == nil, nil
+	}
+}
+
+// ElementIsVisible returns a condition that checks if the element is visible.
+func ElementIsVisible(elt selenium.WebElement) selenium.Condition {
+	return func(wd selenium.WebDriver) (bool, error) {
+		visible, err := elt.IsDisplayed()
+		return visible, err
 	}
 }
--- element_test.go
+++ element_test.go
@@ -39,3 +39,43 @@ func TestElementIsLocated(t *testing.T) {
 		t.Fatalf("wd.Wait() should raise an error, but it didn't.\n")
 	}
 }
+
+func TestElementIsVisible(t *testing.T) {
+	caps := selenium.Capabilities{"browserName": "firefox"}
+	wd, err := selenium.NewRemote(caps, "")
+	defer wd.Quit()
+
+	if err != nil {
+		t.Fatalf("Cannot start selenium.NewRemote(): %v\n", err)
+	}
+
+	// Testing successful selenium.Wait() call.
+	err = wd.Get("http://localhost:3000/element_show")
+	if err != nil {
+		t.Fatalf("Cannot get http://localhost:3000/element_add: %v\n", err)
+	}
+	element, err := wd.FindElement(selenium.ByID, "element")
+	if err != nil {
+		t.Fatalf("Cannot find #element: %v\n", err)
+	}
+
+	// This should not raise an error.
+	if err = wd.Wait(conditions.ElementIsVisible(element)); err != nil {
+		t.Fatalf("Error while executing wd.Wait(): %v\n", err)
+	}
+
+	// Testing unsuccessful selenium.Wait() call (this should raise error cause of timeout).
+	err = wd.Get("http://localhost:3000/element_show")
+	if err != nil {
+		t.Fatalf("Cannot get http://localhost:3000/element_show: %v\n", err)
+	}
+	element2, err := wd.FindElement(selenium.ByID, "element2")
+	if err != nil {
+		t.Fatalf("Cannot find #element2: %v\n", err)
+	}
+
+	// This should raise an timeout error.
+	if err = wd.WaitWithTimeout(conditions.ElementIsVisible(element2), 500*time.Millisecond); err == nil {
+		t.Fatalf("wd.Wait() should raise an error, but it didn't.\n")
+	}
+}
